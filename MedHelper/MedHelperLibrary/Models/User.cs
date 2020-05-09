31a9b64f08278895591128486ae05a37823c27a1
Denis-Bilash <65021230+Denis-Bilash@users.noreply.github.com>
2020-05-09 20:32:43 +0300

    Add files via upload
    Создание библиотеки с объектной моделью.

---
+++ MedHelper/MedHelperLibrary/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedHelper
+{
+    public abstract class User
+    {
+        public string Login { set; get; }
+        public string Password { set; get; }
+        public string Access { get; set; }
+
+    }
+}
